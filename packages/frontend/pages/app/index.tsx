65fd1499ea51fc133b5d6c7656798db4e957f2dc
Nariyuki Saito <toshincompos@gmail.com>
2022-08-09 22:34:28 +0900

    Add form validation with Zora API

--- packages/frontend/pages/app/index.tsx
+++ packages/frontend/pages/app/index.tsx
@@ -1,6 +1,7 @@
 import FormDeployButoton from "../../components/FormDeployButton";
 import FormList from "../../components/FormList";
 
+import { CheckIcon, WarningTwoIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -20,24 +21,90 @@ import {
   ModalHeader,
   ModalOverlay,
   Spacer,
+  Spinner,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import { TEST_NFT_CONTRACT_ADDRESS } from "../../constants/constants";
 import Layout from "../../layouts/default";
+import { fetchNftBaseInfo } from "../../utils/zdk";
+
+const NftName = (props: {
+  nftName: string | null;
+  isLoadingNftName: boolean;
+}) => {
+  if (props.isLoadingNftName) {
+    return <Spinner size="sm" />;
+  } else if (props.nftName == null) {
+    return <></>;
+  } else if (props.nftName.length === 0) {
+    return (
+      <Text color="gray.500" display="flex" alignItems="center">
+        <WarningTwoIcon mr={1} />
+        No NFT project found for the address
+      </Text>
+    );
+  } else {
+    return (
+      <Text display="flex" alignItems="center">
+        <CheckIcon mr={1} fontWeight="100" color="green.500" />
+        Project found: {props.nftName}
+      </Text>
+    );
+  }
+};
 
 const AppRoot = (page: ReactElement) => {
   // Form Dialog
   const initialRef = React.useRef(null);
   const [title, setTitle] = useState("");
   const [contractAddress, setContractAddress] = useState("");
+  const [isLoadingNftName, setIsLoadingNftName] = useState(false);
+  const [loadedNftName, setLoadedNftName] = useState(false);
+  const [nftName, setNftName] = useState<string | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const clickNew = () => {
     setTitle("");
-    setContractAddress(TEST_NFT_CONTRACT_ADDRESS);
+    setContractAddress("");
     onOpen();
   };
+
+  useEffect(() => {
+    (async () => {
+      if (contractAddress.length === 0) {
+        setIsLoadingNftName(false);
+        setNftName(null);
+        setLoadedNftName(false);
+        return;
+      }
+      if (!contractAddress.startsWith("0x") || contractAddress.length !== 42) {
+        setIsLoadingNftName(false);
+        setNftName("");
+        setLoadedNftName(false);
+        return;
+      }
+      if (isLoadingNftName || loadedNftName) return;
+
+      try {
+        setIsLoadingNftName(true);
+        const baseInfo = await fetchNftBaseInfo(contractAddress);
+        if (!baseInfo || !baseInfo.name) {
+          setNftName("");
+          return;
+        }
+
+        setNftName(baseInfo.name);
+      } catch (error: any) {
+        console.error(error);
+        setNftName("");
+      } finally {
+        setIsLoadingNftName(false);
+        setLoadedNftName(true);
+      }
+    })();
+  }, [contractAddress, isLoadingNftName, loadedNftName, nftName]);
 
   return (
     <>
@@ -72,12 +139,12 @@ const AppRoot = (page: ReactElement) => {
 
       <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent color="white" background="gray.700">
           <ModalHeader>Create new form with our template</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
             <FormControl>
-              <FormLabel>Form Name</FormLabel>
+              <FormLabel>Form Title</FormLabel>
               <Input
                 required
                 ref={initialRef}
@@ -87,18 +154,32 @@ const AppRoot = (page: ReactElement) => {
               />
             </FormControl>
             <FormControl mt={4}>
-              <FormLabel>NFT Contract Address</FormLabel>
+              <FormLabel display="flex" justifyContent="stretch" mr="0">
+                NFT Contract Address
+                <Spacer />
+                <Button
+                  size="xs"
+                  onClick={() => setContractAddress(TEST_NFT_CONTRACT_ADDRESS)}
+                  color="gray.500"
+                  borderColor="gray.500"
+                  variant="outline"
+                >
+                  Input test address
+                </Button>
+              </FormLabel>
               <Input
                 required
                 placeholder="0xabcd..."
                 value={contractAddress}
-                onChange={(evt) => setContractAddress(evt.target.value)}
+                onChange={(e) => setContractAddress(e.target.value)}
+                mb={1}
               />
+              <NftName nftName={nftName} isLoadingNftName={isLoadingNftName} />
             </FormControl>
           </ModalBody>
           <ModalFooter>
             <Flex>
-              <Button size="md" variant="text" color="black" onClick={onClose}>
+              <Button size="md" variant="outline" onClick={onClose}>
                 Cancel
               </Button>
               <FormDeployButoton
---
+++ packages/frontend/utils/zdk.ts
@@ -0,0 +1,44 @@
+import { ZDK, ZDKChain, ZDKNetwork } from "@zoralabs/zdk";
+import {
+  TEST_NFT_CONTRACT_ADDRESS,
+  ZORA_DEMO_NFT_ADDRESS,
+} from "../constants/constants";
+
+const networkInfo = {
+  network: ZDKNetwork.Ethereum,
+  chain: ZDKChain.Mainnet,
+};
+const args = {
+  endPoint: "https://api.zora.co/graphql",
+  networks: [networkInfo],
+  includeFullDetails: true,
+};
+const zdk = new ZDK(args);
+
+const nftAddressForDemo = (nftAddress: string) =>
+  nftAddress === TEST_NFT_CONTRACT_ADDRESS ? ZORA_DEMO_NFT_ADDRESS : nftAddress;
+
+export const fetchNftAggregationStats = async (nftAddress: string) => {
+  return await zdk.collectionStatsAggregate({
+    collectionAddress: nftAddressForDemo(nftAddress),
+    network: networkInfo,
+  });
+};
+
+export const fetchNftBaseInfo = async (nftAddress: string) => {
+  return await zdk.collection({
+    address: nftAddressForDemo(nftAddress),
+    includeFullDetails: true,
+  });
+};
+
+export const fetchSampleToken = async (nftAddress: string) => {
+  return await zdk.tokens({
+    where: {
+      collectionAddresses: [nftAddressForDemo(nftAddress)],
+    },
+    pagination: { limit: 1 },
+    includeFullDetails: false,
+    includeSalesHistory: false,
+  });
+};
