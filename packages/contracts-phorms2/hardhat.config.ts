26c9fa425d919d614bb03251c7cdafa2b1f66abb
Nariyuki Saito <toshincompos@gmail.com>
2022-08-21 15:08:25 +0900

    Scaffold contracts-phorms2

---
+++ packages/contracts-phorms2/hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+};
+
+export default config;
