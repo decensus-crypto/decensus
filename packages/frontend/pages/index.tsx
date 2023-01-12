416ef885c95375b0f08c8f1afcfc5ec3e0b8da33
Yosuke Kimoto <yosuke.kmt@gmail.com>
2023-01-12 11:44:24 +0800

    Update Landing Page

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -27,25 +27,14 @@ const Home = () => {
                   <br />
                   Forms
                 </Heading>
-                <Heading
-                  as="h3"
-                  color="white"
-                  size="md"
-                  fontWeight="light"
-                  mt={4}
-                >
+                <Heading as="h3" color="white" size="md" fontWeight="light" mt={4}>
                   Token-gated forms to understand your community
                 </Heading>
               </Box>
               <Box mt={8}>
                 <Flex>
                   <NextLink href="/app">
-                    <Button
-                      width={160}
-                      size="lg"
-                      fontWeight="light"
-                      colorScheme="pink"
-                    >
+                    <Button width={160} size="lg" fontWeight="light" colorScheme="pink">
                       Create Form
                     </Button>
                   </NextLink>
@@ -64,10 +53,7 @@ const Home = () => {
                 </Flex>
               </Box>
             </GridItem>
-            <GridItem
-              colSpan={{ base: 12, md: 6 }}
-              display={{ base: "none", md: "block" }}
-            >
+            <GridItem colSpan={{ base: 12, md: 6 }} display={{ base: "none", md: "block" }}>
               <Image src="/lp.jpg" alt="LP" rounded={12} />
             </GridItem>
           </Grid>
@@ -83,14 +69,13 @@ const Home = () => {
             </GridItem>
             <GridItem colSpan={{ base: 12, md: 6 }}>
               <Text color="white" fontSize="lg" fontWeight="light">
-                Decensus is a free and <b>open-source</b> decentralized
-                application for creating token-gated forms, with privacy in
-                mind. It is designed for communities, decentralized or not, who
-                are interested in gathering explicit information from their
-                members such as opinions, personal information or preferences.
-                Decensus was created by Centiv to address the lack of solutions
-                in this space, and was developed during the Metabolisms&amp; ETH
-                Global hackathon in 2022, where it finished finalist.
+                Decensus is a free and <b>open-source</b> decentralized application for creating
+                token-gated forms, with privacy in mind. It is designed for communities,
+                decentralized or not, who are interested in gathering explicit information from
+                their members such as opinions, personal information or preferences. Decensus was
+                created by Centiv to address the lack of solutions in this space, and was developed
+                during the Metabolisms&amp; ETH Global hackathon in 2022, where it finished
+                finalist.
               </Text>
             </GridItem>
           </Grid>
@@ -102,7 +87,7 @@ const Home = () => {
             <GridItem colSpan={{ base: 12 }}>
               <Heading as="h1" color="white" size="3xl" fontWeight="light">
                 Features
-              </Heading> 
+              </Heading>
             </GridItem>
             <GridItem colSpan={{ base: 12 }}>
               <Grid templateColumns="repeat(12, 1fr)" gap={2}>
@@ -111,19 +96,12 @@ const Home = () => {
                     <Text color="pink.600">
                       <TbShape size={64} />
                     </Text>
-                    <Heading
-                      as="h4"
-                      color="white"
-                      size="md"
-                      fontWeight="bold"
-                      py={4}
-                    >
+                    <Heading as="h4" color="white" size="md" fontWeight="bold" py={4}>
                       Decentralized
                     </Heading>
                     <Text color="white" fontSize="lg" fontWeight="light">
-                      Decensus is built on the Polygon blockchain, benefiting
-                      from censorship-resistance, allowing wallet log in and
-                      unstoppable servicing.
+                      Decensus is built on the Polygon blockchain, benefiting from
+                      censorship-resistance, allowing wallet log in and unstoppable servicing.
                     </Text>
                   </Card>
                 </GridItem>
@@ -132,19 +110,12 @@ const Home = () => {
                     <Text color="pink.600">
                       <TbLockAccess size={64} />
                     </Text>
-                    <Heading
-                      as="h4"
-                      color="white"
-                      size="md"
-                      fontWeight="bold"
-                      py={4}
-                    >
+                    <Heading as="h4" color="white" size="md" fontWeight="bold" py={4}>
                       Token-gated
                     </Heading>
                     <Text color="white" fontSize="lg" fontWeight="light">
-                      Form creators can specify any NFT collection that form
-                      respondents must be holders of in order to access the
-                      form.
+                      Form creators can specify any NFT collection that form respondents must be
+                      holders of in order to access the form.
                     </Text>
                   </Card>
                 </GridItem>
@@ -153,21 +124,14 @@ const Home = () => {
                     <Text color="pink.600">
                       <TbCheckbox size={64} />
                     </Text>
-                    <Heading
-                      as="h4"
-                      color="white"
-                      size="md"
-                      fontWeight="bold"
-                      py={4}
-                    >
+                    <Heading as="h4" color="white" size="md" fontWeight="bold" py={4}>
                       Collectible questions and answers
                     </Heading>
                     <Text color="white" fontSize="lg" fontWeight="light">
-                      Forms and answer sets are represented as NFT collections
-                      and items, respectively, making them collectible and
-                      valuable. In addition, all form answers are encrypted and
-                      only visible to the form creator, ensuring privacy for
-                      respondents.
+                      Forms and answer sets are represented as NFT collections and items,
+                      respectively, making them collectible and valuable. In addition, all form
+                      answers are encrypted and only visible to the form creator, ensuring privacy
+                      for respondents.
                     </Text>
                   </Card>
                 </GridItem>
@@ -176,18 +140,12 @@ const Home = () => {
                     <Text color="pink.600">
                       <TbLock size={64} />
                     </Text>
-                    <Heading
-                      as="h4"
-                      color="white"
-                      size="md"
-                      fontWeight="bold"
-                      py={4}
-                    >
+                    <Heading as="h4" color="white" size="md" fontWeight="bold" py={4}>
                       Privacy
                     </Heading>
                     <Text color="white" fontSize="lg" fontWeight="light">
-                      Form answers are encrypted and only visible to the form
-                      creator, ensuring privacy for respondents.
+                      Form answers are encrypted and only visible to the form creator, ensuring
+                      privacy for respondents.
                     </Text>
                   </Card>
                 </GridItem>
